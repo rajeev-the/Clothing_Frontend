d1c532d13d620c0e3d69e88e89e1636a7b347514
Rajeev <rajeevranjan9560807144@gmail.com>
2025-08-25 01:17:26 +0530

    first commit

--- src/Components/TrendingHome.jsx
+++ src/Components/TrendingHome.jsx
@@ -42,7 +42,7 @@ const TrendingHome = () => {
         <>
           {/* Product Grid */}
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6 mt-8">
-            {products?.map((item) => (
+            {products?.slice(0,30).map((item) => (
               <BoxOfProdcuts
                 key={item._id}
                 id={item._id}
